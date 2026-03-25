3e84b5c49a13b9c466322f691c8b553a283357b6
Aizen <bzarakusev@gmail.com>
2026-03-25 14:38:27 +0300

    GS-2-2 cleand structure

--- src/lesson2/lesson2_task1.kt
+++ src/lesson2/lesson2_task1.kt
@@ -1,9 +1,16 @@
 package lesson2
 
 fun main(){
-    val AGE_OF_MAJORITY = 18
-    println("Введите ваш возраст: ")
+//    val AGE_OF_MAJORITY = 18
+//    println("Введите ваш возраст: ")
     var AGE_USER = readln().toInt()
-    var USER_TRUE_FALSE: Boolean = AGE_USER >= AGE_OF_MAJORITY
-    println("Cовершеннолетие достигнуто: $USER_TRUE_FALSE")
+//    var USER_TRUE_FALSE: Boolean = AGE_USER >= AGE_OF_MAJORITY
+//    println("Cовершеннолетие достигнуто: $USER_TRUE_FALSE")
+
+    if(AGE_USER >= 18){
+        println("Добро пожаловать")
+    }
+    else{
+        println("Вход воспрещен подрастите еще")
+    }
 }
