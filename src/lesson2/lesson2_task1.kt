1e16df41af9bfef75a08266212f1af90cba40562
Aizen <bzarakusev@gmail.com>
2026-03-25 14:33:58 +0300

    GS-2-1 cleand structure

---
+++ src/lesson2/lesson2_task1.kt
@@ -0,0 +1,9 @@
+package lesson2
+
+fun main(){
+    val AGE_OF_MAJORITY = 18
+    println("Введите ваш возраст: ")
+    var AGE_USER = readln().toInt()
+    var USER_TRUE_FALSE: Boolean = AGE_USER >= AGE_OF_MAJORITY
+    println("Cовершеннолетие достигнуто: $USER_TRUE_FALSE")
+}
